c1a335954b0c4382b98af8ab511aa46f6c902580
Abhijit125 <singhabhijit125@gmail.com>
2021-10-22 21:56:29 +0530

    commiting Algo1_java file

---
+++ Algo1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Algo1 
+{
+	public static void main(String args[]) {
+		int a = 6;
+		int b = 5;
+		while(b != 0) {
+			int carry = a & b;
+			a = a ^ b;
+			b = carry << 1;
+		}
+		System.out.println(a);
+	}
+	
+}
